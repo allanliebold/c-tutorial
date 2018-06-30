e8af4338b35b4793c7700788ac056fd4c8e4a29c
Allan Liebold <allan.liebold@gmail.com>
2018-06-30 16:14:38 -0700

    Update basics.cc

--- basics.cc
+++ basics.cc
@@ -1,1 +1,11 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+  // We are defining 2 variables
+  int a = 5;
+  int b = 6;
+  
+  int c = a + b;
+  std::cout << "The value of c is " << c;
+}

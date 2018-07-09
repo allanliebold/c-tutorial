903d2ead86372777332a7cb65eea7d934f9c7561
Allan Liebold <allan.liebold@gmail.com>
2018-07-09 07:28:44 -0700

    Update sampleProgram.cc

--- sampleProgram.cc
+++ sampleProgram.cc
@@ -6,4 +6,9 @@ using namespace.std;
 int main()
 {
   float c, f;
+  
+  cout<<"Welcome! This program converts Celsius to Farenheit."<<endl;
+  cout<<"Enter the degrees in Celsius: "<<endl;
+  
+  cin>>c;
 }

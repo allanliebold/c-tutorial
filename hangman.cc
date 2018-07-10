ceaea02fa606f2208e347e079671999e88851d7c
Allan Liebold <allan.liebold@gmail.com>
2018-07-10 09:11:43 -0700

    Update hangman.cc

--- hangman.cc
+++ hangman.cc
@@ -5,5 +5,16 @@
 using namespace std;
 
 int main() {
+  int lives=0;
+  char letter;
+  int none;
+  char wordToGuess[50]={0};
+  char guessedWord[50]={0};
   
+  cout<<"Type out a word to play Hangman: "<<endl;
+  cin>>wordToGuess;
+  
+  while (lives <=5 && strcmp(guessedWord, wordToGuess) != 0) {
+    
+  }
 }

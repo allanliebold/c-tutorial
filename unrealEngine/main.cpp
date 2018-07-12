aba479ddfdefce5ff4b4616126502cb78ffda23f
Allan Liebold <allan.liebold@gmail.com>
2018-07-12 07:11:10 -0700

    Update main.cpp

--- unrealEngine/main.cpp
+++ unrealEngine/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-  constexpr int WORD_LENGTH = 5;
+  constexpr int WORLD_LENGTH = 5;
   cout << "Welcome" << endl;
   cout << "Can you guess the X letter isogram?\n";
   return 0;

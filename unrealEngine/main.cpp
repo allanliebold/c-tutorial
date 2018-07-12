dcd7e030051e5bafa9b4311876d0c8f844d6da7b
Allan Liebold <allan.liebold@gmail.com>
2018-07-12 07:15:25 -0700

    Update main.cpp

--- unrealEngine/main.cpp
+++ unrealEngine/main.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main()
 {
+  // Game introduction
   constexpr int WORD_LENGTH = 5;
   cout << "Welcome" << endl;
   cout << "Can you guess the " << WORD_LENGTH;
   cout << " letter isogram?\n";
+  
+  // Get a guess from the player
+  string Guess = "";
+  cin >> Guess;
+  
   return 0;
 }
